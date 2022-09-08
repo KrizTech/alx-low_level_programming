50959fbec13c412b203d4cafbe8d1d98bd97f824
KrizTech <christiandonatus01@gmail.com>
2022-09-08 16:54:03 -0700

    my put commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
