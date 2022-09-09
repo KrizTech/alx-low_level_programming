c7a13bc0ed9899195e349dc09569ebf02e0011d5
KrizTech <christiandonatus01@gmail.com>
2022-09-08 18:02:04 -0700

    my size commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n" (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n" (unsigned long)sizeof(b));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of s long long int: %lu byte(s)\n", (unsigned long)sizeof(dd
 ));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	Return (0);
+	return (0);
 }
