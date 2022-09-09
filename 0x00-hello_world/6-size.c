a0d4e0e59d90b1d4be1db01e8dfe11eaf51cdcea
KrizTech <christiandonatus01@gmail.com>
2022-09-08 18:59:02 -0700

    size commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
- * return: 0 if exited properly, non-zero otherwise
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("Size of a char: %d bye(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof (int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu bye(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
