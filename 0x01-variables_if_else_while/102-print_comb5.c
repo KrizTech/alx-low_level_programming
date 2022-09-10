f401139357c38fc442f0925077f6835458c15fb2
KrizTech <christiandonatus01@gmail.com>
2022-09-09 19:25:57 -0700

    print 5 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ *main - print a num pair from 00-99 but no repeats (00 01, 00 02,00 03,...)
  *Return: Always 0 (Success)
  */
 
@@ -26,7 +26,7 @@ int main(void)
 					putchar(t);
 					putchar(o);
 
-					if (!((tens == '9' && ones == '8') && (t == '9' && o == '8')))
+					if (!((tens == '9' && ones == '8') && (t == '9' && 0 == '8')))
 						continue;
 					{
 						putchar(',');
