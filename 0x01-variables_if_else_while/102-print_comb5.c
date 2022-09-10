03702d2cc5f313200d41ddfd0f60fc615be93768
KrizTech <christiandonatus01@gmail.com>
2022-09-09 20:16:55 -0700

    print comb5 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ int main(void)
 
 	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 	{
-		for (ones = "0'; ones <= '9'; ones++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
 			for (t = tens; t <= '9'; t++) /*print second of pair*/
 			{
