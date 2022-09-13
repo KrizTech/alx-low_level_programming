c089e90f4edd48cf112ed855413d35a1beba28c6
KrizTech <christiandonatus01@gmail.com>
2022-09-13 13:22:33 -0700

    1-alphabet commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - print lowercase alphabet ten times 
+ */
+
+void print_alphabet_x10(void);
+
