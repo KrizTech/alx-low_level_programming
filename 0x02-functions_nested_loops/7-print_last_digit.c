e1962290cc137598ae5791aeaad47d26f2244b4c
KrizTech <christiandonatus01@gmail.com>
2022-09-13 14:33:42 -0700

    last digit commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of integer
+ * @nld: number's last digit result
+ * Return: The last digit
+ */
+
+int print_last_digit(int nld)
+{
+	int pld;
+
+	pld = (nld % 10)
+
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+
+	_putchar(pld + '0');
+	return (pld);
+}
