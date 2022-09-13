aef4a080a5349f212e94a1dfeaa6ab0bd7f407a0
KrizTech <christiandonatus01@gmail.com>
2022-09-13 13:04:23 -0700

    2print commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,10 @@ void print_alphabet_x10(void)
 
 	for (counter = 1; counter <= 10; counter++)
 	{
-		for (alph = 'a'; alpha <= 'z'; alpha++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 		}
-		_putchar('\n);
+		_putchar('\n');
 	}
 }
