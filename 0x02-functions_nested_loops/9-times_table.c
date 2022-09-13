6df10ac49253a7c1d605a4d072d9ce4c2f540733
KrizTech <christiandonatus01@gmail.com>
2022-09-13 15:14:29 -0700

    time table commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * time_table - print multiplication table
+ */
+
+void time_table(void)
+{
+	int row;
+	int column;
+	int product;
+
+	for (row = 0; row <= 9; column++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = (row * column);
+
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
+			}
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
+
