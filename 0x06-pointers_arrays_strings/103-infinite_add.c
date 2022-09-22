a84bfdf6cf5bcfd38b8c8b00eb16455ad4573a6d
KrizTech <christiandonatus01@gmail.com>
2022-09-22 02:59:54 -0700

    infinite commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,6 +9,7 @@
  * @size_r: buffer size
  * Return: the pointer to dest
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 {
 	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
@@ -34,7 +35,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 		else
 			add = 0;
 		if (op > 0)
-			*(r + bg) = (op % 10) + 48;
+		*(r + bg) = (op % 10) + 48;
 		else
 			*(r + bg) = '0';
 		if (c1 > 0)
