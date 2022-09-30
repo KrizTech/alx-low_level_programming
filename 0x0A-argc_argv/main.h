421cd11aa43be703d3fe244998b2c2d8ae863219
KrizTech <christiandonatus01@gmail.com>
2022-09-30 15:29:24 -0700

    args commit

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,7 @@
 
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
 int _putchar(char c);
 int _atoi(char *s);
 
