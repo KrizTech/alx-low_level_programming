cf11805e0aacf3007f44ca7e253b85ce975a73a4
KrizTech <christiandonatus01@gmail.com>
2022-10-31 16:27:22 -0700

    elf header commit

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -185,7 +185,6 @@ void print_osabi(unsigned char *e_ident)
  * print_abi - Prints the ABI version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF ABI version.
  */
-
 void print_abi(unsigned char *e_ident);
 {
 	printf("  ABI Version:                       %d\n",
@@ -197,7 +196,6 @@ void print_abi(unsigned char *e_ident);
  * @e_type: The ELF type.
  * @e_ident: A pointer to an array containing the ELF class.
  */
-
 void print_type(unsigned int e_type, unsigned char *e_ident);
 {
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
@@ -232,7 +230,6 @@ void print_type(unsigned int e_type, unsigned char *e_ident);
  * @e_entry: The address of the ELF entry point.
  * @e_ident: A pointer to an array containing the ELF class.
  */
-
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 {
 	printf("  Entry point address:               ");
@@ -257,7 +254,6 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
  *
  * Description: If the file cannot be closed - exit code 98.
  */
-
 void close_elf(int elf);
 {
 	if (close(elf) == -1)
@@ -279,7 +275,6 @@ void close_elf(int elf);
  * Description: If the file is not an ELF File or
  *              the function fails - exit code 98.
  */
-
 int main(int __attribute__((__unused__)) argc, char *argv[]);
 {
 	Elf64_Ehdr *header;
