39a3d35583360740adb60ca82c9469b9da004830
KrizTech <christiandonatus01@gmail.com>
2023-04-12 05:25:54 -0700

    Search Algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,9 @@
-#ifndef _SORTING_ALGOS_H_
-#define _SORTING_ALGOS_H_
+#ifndef SEARCH_H
+#define SEARCH_H
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 /**
  * struct listint_s - singly linked list
@@ -40,14 +41,58 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
+/*
+ * Function that searches for a value in an
+ * array of integers using the Linear search algorithm
+ */
 int linear_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a sorted array
+ * of integers using the Binary search algorithm
+ */
 int binary_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a
+ * sorted array of integers using the Jump search algorithm
+ */
 int jump_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a sorted array of
+ * integers using the Interpolation search algorithm
+ */
 int interpolation_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a sorted array
+ * of integers using the Exponential search algorithm
+ */
 int exponential_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a
+ * sorted array of integers
+ */
 int advanced_binary(int *array, size_t size, int value);
-int rec_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a sorted list
+ * of integers using the Jump search algorithm in a singly linked list
+ */
 listint_t *jump_list(listint_t *list, size_t size, int value);
+
+ /* print_list - Prints the content of a listint_t */
+void print_list(const listint_t *list);
+
+/* create_list - Creates a single linked list */
+listint_t *create_list(int *array, size_t size);
+
+/* free_list - Deallocates a singly linked list */
+void free_list(listint_t *list);
+
+/* function that searches for a value in a sorted skip list of integers */
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
-#endif
+#endif /* SEARCH_H */
